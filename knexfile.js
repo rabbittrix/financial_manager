d3f99bbf0fb696f87a260a25e7ae6a903fef0809
carcloud2020 <rabbittrix@hotmail.com>
2020-07-16 16:37:40 +0100

    Tests

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,24 @@ module.exports = {
       database: "financial",
     },
     migrations: {
-      directory: "src/migrations",
+      directory: "src/migrations"      
     },
+    seeds: {
+      directory: "src/seeds"
+    },
+  },
+
+  prod: {
+    client: "pg",
+    version: "8.2.1",
+    connection: {
+      host: "localhost",
+      user: "postgres",
+      password: "admin",
+      database: "bank_financial",
+    },
+    migrations: {
+      directory: "src/migrations"
+    },    
   },
 };
--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+const winston = require("winston");
+const uuid = require("uuidv4");
 const knexfile = require('../knexfile');
-const { query } = require('express');
+//const { query } = require('express');
 //const knexLogger = require('knex-logger');
 
 // TODO create dynamic switching
-app.db = knex(knexfile.test);
+//app.db = knex(knexfile[process.env.NODE_ENV]);
+//app.db = knex(knexfile.test);
 
 //app.use(knexLogger(app.db));
+
+app.log = winston.createLogger({
+  level: "debug",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.json({ space: 1 }),
+    }),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "warn",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json({ space: 1 })
+      ),
+    }),
+  ],
+});
 
 consign({
     cwd: 'src',
@@ -23,6 +43,19 @@ consign({
 
 app.get('/', (req, res) => {
   res.status(200).send();
+});
+app.use((err, req, res, next) => {
+  const { name, message, stack } = err;
+  if (name === "ValidationError") res.status(400).json({ error: message });
+  else if (name === "RecursoIndevidoError")
+    res.status(403).json({ error: message });
+  else {
+    const id = uuid();
+    // console.log(message);
+    app.log.error({ id, name, message, stack });
+    res.status(500).json({ id, error: "Falha interna" });
+  }
+  next(err);
 });
 /*
 // show querys on console
