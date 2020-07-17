6c79ff329e97993e46fd1c28ee5f2f1f1f8963ef
carcloud2020 <rabbittrix@hotmail.com>
2020-07-17 15:57:10 +0100

    Install cors

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = (app) => {
   app.use(bodyParser.json());
-}
+
+  app.user(cors({ origin: '*' }));
+};
 
